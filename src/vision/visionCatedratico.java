b0521b2e02597a34e9c32085b731a441428aeccd
alexandrorl373 <josealex2004@hotmail.com>
2023-04-16 23:31:47 -0600

    Modificacion en las clases catedratico By: alexandro_dev

---
+++ src/vision/visionCatedratico.java
@@ -0,0 +1,35 @@
+package vision;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class visionCatedratico extends JFrame {
+    private JPanel panelPrincipal;
+    private JLabel lblNombre;
+    private JLabel lblRFC;
+    public  JTextField txtNombre;
+    public  JTextField txtRFC;
+    public JButton salirButton;
+    public JButton jbGuardar;
+    public  JButton jbSalir;
+
+    public visionCatedratico(){
+        setTitle("Registro Catedratico");
+        setContentPane(panelPrincipal);
+        setSize(500, 300);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        setVisible(false);
+        jbSalir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                visionMenu Menu= new visionMenu();
+                Menu.setVisible(true);
+                dispose();
+            }
+        });
+    }
+
+
+}
