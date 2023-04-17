2245b47dc7df30d3ca9e2aff629829a7faaaf0b5
abraham <L21021172@veracruz.tecnm.mx>
2023-04-17 13:37:48 -0600

    integracion de las clases materias v2.0

---
+++ src/modelo/modeloMateria.java
@@ -0,0 +1,41 @@
+package modelo;
+
+public class modeloMateria {
+
+
+    private int id;
+    private String nombre;
+
+    public modeloMateria() {
+    }
+
+    public modeloMateria(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "modeloMateria{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+
+}
